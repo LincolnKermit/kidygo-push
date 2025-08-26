3253eb1a2b62b967e8661821c39b82240679ee6a
Lincoln K. <104798220+LincolnKermit@users.noreply.github.com>
2025-08-26 09:39:42 +0200

    Update lib.py

--- lib.py
+++ lib.py
@@ -5,12 +5,13 @@ def remove_double(list_data):
 
 
 class config:
-    def get_data():
-        # get data from config.json
-        with open('config.json') as f:
-            data = json.load(f)
-            departure = data['config']['departure']
-            arrival = data['config']['arrival']
-            is_transversal = data['config']['is_transversal']
-            delta = data['config']['delta']
-            return departure, arrival, is_transversal, delta
+    with open('config.json') as f:
+        data = json.load(f)
+        def departure():
+            return data['config']['departure']
+        def arrival():
+            return data['config']['arrival']
+        def is_tranversal():
+            return data['config']['is_transversal']
+        def delta():
+            return data['config']['delta']
